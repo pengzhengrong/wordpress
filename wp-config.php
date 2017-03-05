bdb99b5d5b3500a2c12254a57fb4a8897ea70648
pzrPzrzy <pzr@pzrzydeMacBook-Pro.local>
2017-03-05 17:18:16 +0800

    修改db配置

--- wp-config.php
+++ wp-config.php
@@ -19,17 +19,18 @@
  */
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
+
 /** WordPress数据库的名称 */
 define('DB_NAME', 'blog');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', $_SERVER['DB_USER']);
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', $_SERVER['DB_PWD']);
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', $_SERVER['DB_HOST']);
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8mb4');
